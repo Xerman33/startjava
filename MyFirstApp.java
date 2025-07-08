a8cf7759388bfd7f3174363c66a021ca173d2c4f
Xerman33 <xerman1991@gmail.com>
2025-07-08 15:31:46 +0300

    Добавил .gitignore с шаблоном *.class

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -1,6 +1,16 @@
-public class MyFirstApp {   
+import java.util.Scanner;
+
+public class MyFirstApp {
     public static void main(String[] args) {
-        System.out.print("Hello! Это программа AC FirMan!");
-        System.out.println("Она будет помогать людям");
+        System.out.println("У какого языка программирования следующий слоган:");
+        System.out.print("\"Написано однажды, ");
+        System.out.println("работает везде!\"");
+
+        String answer = new Scanner(System.in).next();
+        if (answer.equalsIgnoreCase("Java")) {
+            System.out.println("Вы угадали");
+        } else {
+            System.out.println("Увы, но - это Java");
+        }
     }
 }
